69aeb2a0297f1cf937d14e064ce41ee2494a1620
Sunil-731820 <sunilg@infinite.com>
2026-03-18 11:43:20 +0530

    SolveX And SolveXStream is Added

---
+++ SolveXStream/src/main/java/com/java/stream/SolveXStream/Application.java
@@ -0,0 +1,13 @@
+package com.java.stream.SolveXStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
